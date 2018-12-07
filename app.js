7d9743a291a43aaba491f721138a752e4809d2ec
Venkateshl <venkatesh.lekkala@ggktech.com>
2018-12-07 18:14:21 +0530

    Integrated MongoDB

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+var MongoClient = require('mongodb').MongoClient;
+var config = require('./config.json');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -20,22 +21,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Connect to the db
+MongoClient.connect(config.MONGODB_URL, function (err, db) {
+  if (err) {
+    throw err
+  } else {
+    console.log('Connected to MongoDB');
+  }
 });
 
 module.exports = app;

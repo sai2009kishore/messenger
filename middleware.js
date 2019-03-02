ad520faac42e624ccf2b7ed3b12c2981d3dcc3ff
Sai Kishore <sai2009kishore@gmail.com>
2019-03-02 16:16:33 +0530

    Skipping validation for pre-flight requests

--- middleware.js
+++ middleware.js
@@ -2,14 +2,18 @@ var { verifyJWTToken } = require('./libs/auth');
 
 function verifyJWT_MW(req, res, next) {
     let token = req.get('jwt-token');
-    verifyJWTToken(token)
-        .then((decodedToken) => {
-            next()
-        })
-        .catch((err) => {
-            res.status(401)
-                .json({ message: "You are not authorized to access this link" })
-        })
+    if (req.method === 'OPTIONS') {
+        res.status(200).send();
+    } else {
+        verifyJWTToken(token)
+            .then((decodedToken) => {
+                next()
+            })
+            .catch((err) => {
+                res.status(401)
+                    .json({ message: "You are not authorized to access this link" })
+            })
+    }
 }
 
 module.exports = {

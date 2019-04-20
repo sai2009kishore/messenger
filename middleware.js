e2b63cbd3db3b288157e8e4963e50bddc243ffb7
Kishore <sai2009kishore@gmail.com>
2019-04-20 15:34:35 +0530

    configuration changes

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 var { verifyJWTToken } = require('./libs/auth');
 
 function verifyJWT_MW(req, res, next) {
-    let token = req.get('jwt-token');
+    let token = req.get('Authorization');
     if (req.method === 'OPTIONS') {
         res.status(200).send();
     } else {
